3c2eb683af99cb3ad9577ec8eb28aef37d037f9b
Daiki Tamura <dtamura23@outlook.jp>
2020-06-25 19:38:31 +0900

    hoge

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ data "archive_file" "test" {
   source_dir = "./src"
   output_path = "./dist/test.zip"
 }
+
+data "archive_file" "test2" {
+  type        = "zip"
+  source_dir = "./dist"
+  output_path = "./dist2/test.zip"
+}
